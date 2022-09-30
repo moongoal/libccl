802388270b55d80524c6118a4c182937dfd061a2
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-09-30 09:09:11 +0100

    Added allocator concepts

---
+++ include/charcoal/concepts.hpp
@@ -0,0 +1,51 @@
+/**
+ * @file
+ *
+ * Concept definitions.
+ */
+#ifndef CCL_CONCEPTS_HPP
+#define CCL_CONCEPTS_HPP
+
+#include <type_traits>
+#include <concepts>
+
+namespace ccl {
+    /**
+     * An allocator able to perform byte allocate/free operations.
+     *
+     * This concept does not requried the templated `allocate()`
+     * member function to be defined.
+     *
+     * @tparam Allocator The allocator type.
+     */
+    template<typename Allocator>
+    concept basic_allocator = requires(
+        Allocator alloc,
+        const size_t n_bytes,
+        const size_t alignment,
+        const int flags,
+        void * const ptr
+    ) {
+        { alloc.allocate(n_bytes, flags) } -> std::convertible_to<void*>;
+        { alloc.allocate(n_bytes, alignment, flags) } -> std::convertible_to<void*>;
+
+        alloc.free(ptr);
+    };
+
+    /**
+     * An allocator able to perform typed allocate/free operations.
+     *
+     * This concept is similar to base_allocator but does also requrie
+     * the templated `allocate()` member function to be defined.
+     *
+     * @tparam Allocator The allocator type.
+     */
+    template<typename Allocator, typename T>
+    concept typed_allocator = requires(Allocator allocator, const size_t n, const int flags) {
+        requires basic_allocator<Allocator>;
+
+        { allocator.allocate(n, flags) } -> std::convertible_to<T*>;
+    };
+}
+
+#endif // CCL_CONCEPTS_HPP
--- include/charcoal/vector.hpp
+++ include/charcoal/vector.hpp
@@ -7,11 +7,44 @@
 #define CCL_VECTOR_HPP
 
 #include <charcoal/api.hpp>
+#include <charcoal/allocator.hpp>
+#include <charcoal/concepts.hpp>
 
 namespace ccl {
+    template<
+        typename T,
+        typed_allocator<T> Allocator
+    >
     class vector {
+        public:
+            using size_type = size_t;
+            using value_type = T;
+            using allocator_type = Allocator;
+
+        private:
+            size_type size = 0;
+            size_type capacity = 0;
+            value_type * data = nullptr;
+            allocator_type * allocator = nullptr;
 
         public:
+            explicit constexpr vector(
+                allocator_type * const allocator = nullptr
+            ) : allocator{allocator ? allocator : get_default_allocator<value_type>()}
+            {}
+
+            void reserve(const size_t new_capacity) {
+                if(new_capacity > capacity) {
+                    value_type * const new_data = allocator->template allocate<value_type>(new_capacity);
+
+                    for(size_t i = 0; i < size; ++i) {
+                        new_data[i] = std::move(data[i]);
+                    }
+
+                    allocator->free(data);
+                    data = new_data;
+                }
+            }
     };
 }
 
