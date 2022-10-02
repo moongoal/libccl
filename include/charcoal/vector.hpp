252baa0fd619a7ad5feb50d7f45a46a5e55b3181
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-10-02 15:10:49 +0100

    Added vector iterator

--- include/charcoal/vector.hpp
+++ include/charcoal/vector.hpp
@@ -10,11 +10,103 @@
 #include <memory>
 #include <cassert>
 #include <functional>
+#include <iterator>
+#include <vector>
 #include <charcoal/api.hpp>
 #include <charcoal/allocator.hpp>
 #include <charcoal/concepts.hpp>
 
 namespace ccl {
+    template<typename Vector>
+    struct vector_iterator {
+        using iterator_category = std::contiguous_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = typename Vector::value_type;
+        using pointer = typename Vector::pointer;
+        using reference = typename Vector::reference;
+
+        constexpr vector_iterator(const pointer ptr = nullptr) noexcept : ptr{ptr} {}
+        constexpr vector_iterator(const vector_iterator &other) noexcept : ptr{other.ptr} {}
+        constexpr vector_iterator(vector_iterator &&other) noexcept : ptr{std::move(other.ptr)} {}
+
+        constexpr vector_iterator& operator =(const vector_iterator &other) noexcept {
+            ptr = other.ptr;
+
+            return *this;
+        }
+
+        constexpr vector_iterator& operator =(vector_iterator &&other) noexcept {
+            ptr = std::move(other.ptr);
+
+            return *this;
+        }
+
+        constexpr reference operator*() const noexcept { return *ptr; }
+        constexpr reference operator->() const noexcept { return *ptr; }
+
+        constexpr vector_iterator& operator +=(const difference_type n) noexcept {
+            ptr += n;
+            return *this;
+        }
+
+        constexpr vector_iterator& operator -=(const difference_type n) noexcept {
+            ptr -= n;
+            return *this;
+        }
+
+        constexpr vector_iterator operator +(const difference_type n) const noexcept {
+            return ptr + n;
+        }
+
+        constexpr vector_iterator operator -(const difference_type n) const noexcept {
+            return ptr - n;
+        }
+
+        constexpr difference_type operator -(const vector_iterator other) const noexcept {
+            return ptr - other.ptr;
+        }
+
+        constexpr reference operator[](const difference_type i) const noexcept {
+            return ptr[i];
+        }
+
+        constexpr bool operator ==(const vector_iterator other) const noexcept { return ptr == other.ptr;}
+        constexpr bool operator !=(const vector_iterator other) const noexcept { return ptr != other.ptr;}
+        constexpr bool operator >(const vector_iterator other) const noexcept { return ptr > other.ptr;}
+        constexpr bool operator <(const vector_iterator other) const noexcept { return ptr < other.ptr;}
+        constexpr bool operator >=(const vector_iterator other) const noexcept { return ptr >= other.ptr;}
+        constexpr bool operator <=(const vector_iterator other) const noexcept { return ptr <= other.ptr;}
+
+        constexpr vector_iterator& operator --() const noexcept {
+            --ptr;
+            return *this;
+        }
+
+        constexpr vector_iterator operator --(int) const noexcept {
+            return ptr--;
+        }
+
+        constexpr vector_iterator& operator ++() const noexcept {
+            ++ptr;
+            return *this;
+        }
+
+        constexpr vector_iterator operator ++(int) const noexcept {
+            return ptr++;
+        }
+
+        private:
+            pointer ptr;
+    };
+
+    template<typename Vector>
+    constexpr vector_iterator<Vector> operator +(
+        const typename vector_iterator<Vector>::difference_type n,
+        const vector_iterator<Vector> it
+    ) noexcept {
+        return vector_iterator<Vector>{it.get_data()};
+    }
+
     template<
         typename T,
         typed_allocator<T> Allocator
@@ -25,7 +117,12 @@ namespace ccl {
         public:
             using size_type = size_t;
             using value_type = T;
+            using pointer = T*;
+            using reference = T&;
+            using const_reference = const T&;
             using allocator_type = Allocator;
+            using iterator = vector_iterator<vector>;
+            using const_iterator = vector_iterator<vector<const value_type, allocator_type>>;
 
         private:
             size_type length = 0;
@@ -135,6 +232,11 @@ namespace ccl {
 
                 return data[index];
             }
+
+            constexpr iterator begin() const noexcept { return data; }
+            constexpr const_iterator cbegin() const noexcept { return data; }
+            constexpr iterator end() const noexcept { return data + length; }
+            constexpr const_iterator cend() const noexcept { return data + length; }
     };
 }
 
