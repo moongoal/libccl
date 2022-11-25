cb9f52616e0d45e077f255edb5b08b078950e6c8
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-11-25 00:45:28 +0000

    Added bitset

--- include/ccl/bitset.hpp
+++ include/ccl/bitset.hpp
@@ -14,6 +14,9 @@ namespace ccl {
     template<typed_allocator<uint64_t> Allocator>
     struct bitset_iterator;
 
+    /**
+     * A variable sequence of bits.
+     */
     template<typed_allocator<uint64_t> Allocator = allocator>
     class bitset {
         public:
@@ -31,28 +34,74 @@ namespace ccl {
                 destroy();
             }
 
+            /**
+             * Clear this bitset, removing all bits.
+             */
             void clear() {
                 clusters.clear();
                 _size_bits = 0;
             }
 
+            /**
+             * Release any resources.
+             */
             void destroy() noexcept {
                 clusters.destroy();
                 _size_bits = 0;
             }
 
+            /**
+             * Get the size of the bitset.
+             *
+             * @return The size as number of bits.
+             */
             constexpr size_type size_bits() const noexcept {
                 return _size_bits;
             }
 
+            /**
+             * Get the size of the bitset.
+             *
+             * @return The size as number of clusters.
+             */
             constexpr size_type size() const noexcept {
                 return clusters.size();
             }
 
+            /**
+             * Get the capacity of the bitset.
+             *
+             * @return The capacity as number of clusters.
+             */
             constexpr size_type capacity() const noexcept {
                 return clusters.capacity();
             }
 
+            /**
+             * Reserve some capacity.
+             *
+             * @param The new suggested capacity, in bits.
+             */
+            constexpr void reserve(const size_type new_capacity) {
+                clusters.reserve(new_capacity >> 3 >> (bitcount(sizeof(cluster_type)) - 1));
+            }
+
+            /**
+             * Resize the bitset. When growing, new bits have
+             * undefined value.
+             *
+             * @param new_size The new size of the collection, in bits.
+             */
+            constexpr void resize(const size_type new_size) {
+                clusters.resize((new_size >> 3) + 1);
+                _size_bits = new_size;
+            }
+
+            /**
+             * Append a new bit.
+             *
+             * @param value The to assign.
+             */
             constexpr void push_back(const bool value) {
                 const auto [target_cluster_index, internal_bit_index] = locate_bit(_size_bits);
                 const size_type required_cluster_size = target_cluster_index + 1;
@@ -69,7 +118,10 @@ namespace ccl {
                 _size_bits++;
             }
 
-            constexpr void push_back_true() {
+            /**
+             * Append a new set bit.
+             */
+            constexpr void push_back_set() {
                 const auto [target_cluster_index, internal_bit_index] = locate_bit(_size_bits);
                 const size_type required_cluster_size = target_cluster_index + 1;
 
@@ -80,7 +132,10 @@ namespace ccl {
                 _size_bits++;
             }
 
-            constexpr void push_back_false() {
+            /**
+             * Append a new clear bit.
+             */
+            constexpr void push_back_clear() {
                 const auto [target_cluster_index, internal_bit_index] = locate_bit(_size_bits);
                 const size_type required_cluster_size = target_cluster_index + 1;
 
@@ -91,6 +146,13 @@ namespace ccl {
                 _size_bits++;
             }
 
+            /**
+             * Access a bit given its index.
+             *
+             * @param index The index of the bit to access.
+             *
+             * @return The value of the bit at that index.
+             */
             constexpr bool operator[](const size_type index) const {
                 CCL_THROW_IF(index >= _size_bits, std::out_of_range{"Index out of range."});
 
@@ -100,6 +162,11 @@ namespace ccl {
                 return target_cluster & static_cast<cluster_type>(1) << internal_bit_index;
             }
 
+            /**
+             * Set a bit given its index.
+             *
+             * @param index The index of the bit to set.
+             */
             constexpr void set(const size_type index) {
                 CCL_THROW_IF(index >= _size_bits, std::out_of_range{"Index out of range."});
 
@@ -108,6 +175,11 @@ namespace ccl {
                 target_cluster |= static_cast<cluster_type>(1) << internal_bit_index;
             }
 
+            /**
+             * Clear a bit given its index.
+             *
+             * @param index The index of the bit to clear.
+             */
             constexpr void clear(const size_type index) {
                 CCL_THROW_IF(index >= _size_bits, std::out_of_range{"Index out of range."});
 
@@ -116,6 +188,12 @@ namespace ccl {
                 target_cluster &= ~(static_cast<cluster_type>(1) << internal_bit_index);
             }
 
+            /**
+             * Assign a value to a bit, given its index.
+             *
+             * @param index The index of the bit to assign.
+             * @param value The value to assign the index.
+             */
             constexpr void assign(const size_type index, const bool value) {
                 CCL_THROW_IF(index >= _size_bits, std::out_of_range{"Index out of range."});
 
@@ -131,18 +209,45 @@ namespace ccl {
             }
 
             private:
+                /**
+                 * The sequence of clusters containing the bits.
+                 */
                 vector<cluster_type, allocator_type> clusters;
+
+                /**
+                 * Bit count.
+                 */
                 size_type _size_bits;
 
+                /**
+                 * Representation of the location of a bit within `clusters`.
+                 */
                 struct bit_location {
+                    /**
+                     * The index of the cluster where the bit is located.
+                     */
                     size_type target_cluster_index;
+
+                    /**
+                     * The bit location within the cluster.
+                     */
                     size_type internal_bit_index;
                 };
 
+                /**
+                 * Compute the location of a bit in terms of cluster index
+                 * and bit index within the cluster.
+                 *
+                 * @param The bit index.
+                 *
+                 * @return The bit location coordinates within `clusters`.
+                 */
                 static constexpr bit_location locate_bit(const size_type index) {
+                    const size_type cluster_bit_count = bitcount(sizeof(cluster_type));
+
                     return {
-                        index / sizeof(cluster_type),
-                        index % sizeof(cluster_type)
+                        index >> cluster_bit_count,
+                        index & (cluster_bit_count - 1)
                     };
                 }
     };
--- test/bitset.cpp
+++ test/bitset.cpp
@@ -1,23 +1,24 @@
 #include <ccl/test.hpp>
 #include <ccl/bitset.hpp>
+#include <ccl/util.hpp>
 
 using namespace ccl;
 
 int main(int argc, char **argv) {
-    suite.add_test("push_back_true", [] () {
+    suite.add_test("push_back_set", [] () {
         bitset x;
 
-        x.push_back_true();
+        x.push_back_set();
 
         check(x.size() == 1);
         check(x.size_bits() == 1);
         check(x[0] == true);
     });
 
-    suite.add_test("push_back_false", [] () {
+    suite.add_test("push_back_clear", [] () {
         bitset x;
 
-        x.push_back_false();
+        x.push_back_clear();
 
         check(x.size() == 1);
         check(x.size_bits() == 1);
@@ -39,7 +40,7 @@ int main(int argc, char **argv) {
     suite.add_test("operator[] (out of bounds)", [] () {
         bitset x;
 
-        throws<std::out_of_range>([x] () {
+        throws<std::out_of_range>([&x] () {
             x[0];
         });
     });
@@ -59,7 +60,7 @@ int main(int argc, char **argv) {
     suite.add_test("set", [] () {
         bitset x;
 
-        x.push_back_false();
+        x.push_back_clear();
         x.set(0);
 
         check(x.size() == 1);
@@ -70,7 +71,7 @@ int main(int argc, char **argv) {
     suite.add_test("clear", [] () {
         bitset x;
 
-        x.push_back_true();
+        x.push_back_set();
         x.clear(0);
 
         check(x.size() == 1);
@@ -81,12 +82,69 @@ int main(int argc, char **argv) {
     suite.add_test("assign", [] () {
         bitset x;
 
-        x.push_back_true();
+        x.push_back_set();
         x.assign(0, false);
 
         check(x.size() == 1);
         check(x.size_bits() == 1);
         check(x[0] == false);
+    });
+
+    suite.add_test("reserve (grow)", [] () {
+        bitset x;
+
+        x.push_back_set();
+        x.reserve(sizeof(bitset<>::cluster_type) * 8 /* bits */ * 2 /* two clusters of bits */);
+
+        check(x.capacity() == 2);
+        check(x.size() == 1);
+        check(x.size_bits() == 1);
+        check(x[0] == true);
+    });
+
+    suite.add_test("reserve (shrink)", [] () {
+        bitset x;
+
+        x.push_back_set();
+        x.reserve(0);
+
+        check(x.capacity() == 1);
+        check(x.size() == 1);
+        check(x.size_bits() == 1);
+        check(x[0] == true);
+    });
+
+    suite.add_test("reserve (same value)", [] () {
+        bitset x;
+
+        x.push_back_set();
+        x.reserve(sizeof(bitset<>::cluster_type) * 8 /* bits */);
+
+        check(x.capacity() == 1);
+        check(x.size() == 1);
+        check(x.size_bits() == 1);
+        check(x[0] == true);
+    });
+
+    suite.add_test("resize (grow)", [] () {
+        bitset x;
+
+        x.push_back_set();
+        x.resize(2);
+
+        x[1];
+    });
+
+    suite.add_test("resize (shrink)", [] () {
+        bitset x;
+
+        x.push_back_set();
+        x.push_back_set();
+        x.resize(1);
+
+        throws<std::out_of_range>([&x] () {
+            x[1];
+        });
     });
 
     return suite.main(argc, argv);
