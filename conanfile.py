6cb3ad1b2b2cbaf7f5dec2fe7e65928f5824f834
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-12-03 16:16:37 +0000

    Updated conanfile to include cmake files

--- conanfile.py
+++ conanfile.py
@@ -34,12 +34,15 @@ class LibcclConan(ConanFile):
         cmake.configure()
         cmake.build()
         cmake.test()
+        cmake.install()
 
     def package_id(self):
         self.info.header_only()
 
     def package(self):
         self.copy("*.hpp", dst="include", src="include")
+        self.copy("ccl-config.cmake")
+        self.copy("ccl-config-version.cmake")
 
     def package_info(self):
         self.cpp_info.libs = ["libccl"]
