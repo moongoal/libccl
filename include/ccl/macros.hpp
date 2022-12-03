e50afb2e770a149ace1cf3805240b4c2508f6412
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-12-03 15:23:53 +0000

    Added version and macro headers

---
+++ include/ccl/macros.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file
+ *
+ * Macro definitions.
+ */
+#ifndef CCL_MACROS_HPP
+#define CCL_MACROS_HPP
+
+#define CCL_STRINGIFY(s) CCL_STRINGIFY_INTERNAL(s)
+#define CCL_STRINGIFY_INTERNAL(s) # s
+
+#endif // CCL_MACROS_HPP
