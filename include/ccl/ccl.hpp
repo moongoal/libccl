4c408438585b04357a09f45133d59bb892611ba7
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-12-04 09:27:28 +0000

    Added main headers

---
+++ include/ccl/ccl.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file
+ *
+ * CCL base library.
+ */
+#ifndef CCL_HPP
+#define CCL_HPP
+
+#include <ccl/api.hpp>
+#include <ccl/bitset.hpp>
+#include <ccl/compressed-pair.hpp>
+#include <ccl/concepts.hpp>
+#include <ccl/debug.hpp>
+#include <ccl/definitions.hpp>
+#include <ccl/exceptions.hpp>
+#include <ccl/features.hpp>
+#include <ccl/hash.hpp>
+#include <ccl/hashtable.hpp>
+#include <ccl/macros.hpp>
+#include <ccl/maybe.hpp>
+#include <ccl/set.hpp>
+#include <ccl/test.hpp>
+#include <ccl/util.hpp>
+#include <ccl/vector.hpp>
+#include <ccl/version.hpp>
+
+#include <ccl/memory/memory.hpp>
+#include <ccl/tables/tables.hpp>
+
+#endif // CCL_HPP
---
+++ include/ccl/memory/memory.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * Memory management.
+ */
+#ifndef CCL_MEMORY_HPP
+#define CCL_MEMORY_HPP
+
+#include <ccl/memory/allocator.hpp>
+#include <ccl/memory/local-allocator.hpp>
+#include <ccl/memory/composite-allocator.hpp>
+
+#endif // CCL_MEMORY_HPP
