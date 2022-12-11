55d544e5fb8ac74804bcb0fbbb015ef1cefab126
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2022-12-11 21:13:16 +0000

    Added packed integer

---
+++ include/ccl/packed-integer.hpp
@@ -0,0 +1,123 @@
+/**
+ * @file
+ *
+ * A packed integer is made of two integers fitting the same
+ * memory location.
+ */
+#ifndef CCL_PACKED_INTEGER_HPP
+#define CCL_PACKED_INTEGER_HPP
+
+#include <ccl/api.hpp>
+#include <ccl/util.hpp>
+#include <ccl/concepts.hpp>
+#include <ccl/exceptions.hpp>
+
+namespace ccl {
+    /**
+     * A single integer packing two values as "high" part
+     * and "low" part.
+     *
+     * @tparam T The underlying unsigned integer type.
+     * @tparam LowPartSize The size in bits of the lower part.
+     */
+    template<std::unsigned_integral T, size_t LowPartSize = sizeof(T) * 4>
+    class packed_integer {
+        static_assert(LowPartSize > 0 && LowPartSize < sizeof(T) * 8, "LowPartSize out of bounds.");
+
+        public:
+            using value_type = T;
+
+            /**
+             * The size of the low part, in bits.
+             */
+            static constexpr size_t low_part_size = LowPartSize;
+
+            /**
+             * The number of bits to shift the value to retrieve the high part.
+             */
+            static constexpr size_t high_part_shift_bits = LowPartSize;
+
+            /**
+             * The mask to apply to the value to retrieve the low part.
+             */
+            static constexpr value_type low_part_mask = ~(~static_cast<value_type>(0) >> high_part_shift_bits << high_part_shift_bits);
+
+            /**
+             * Maximum value for the low part.
+             */
+            static constexpr value_type low_part_max = low_part_mask;
+
+            /**
+             * Maximum value for the high part.
+             */
+            static constexpr value_type high_part_max = (~low_part_max) >> high_part_shift_bits;
+
+        private:
+            value_type value;
+
+        public:
+            constexpr packed_integer() : value{static_cast<T>(0)} {}
+            constexpr packed_integer(const packed_integer &) = default;
+            constexpr packed_integer(const value_type value) : value{value} {}
+
+            constexpr operator value_type() const { return value; }
+            constexpr packed_integer& operator =(const packed_integer &) = default;
+
+            constexpr packed_integer& operator =(const value_type value) {
+                this->value = value;
+
+                return *this;
+            }
+
+            /**
+             * Get the high part.
+             *
+             * @return The value of the high part.
+             */
+            constexpr value_type high() const {
+                return value >> high_part_shift_bits;
+            }
+
+            /**
+             * Get the low part.
+             *
+             * @return The value of the low part.
+             */
+            constexpr value_type low() const {
+                return value & low_part_mask;
+            }
+
+            /**
+             * Get the whole value.
+             *
+             * @return The full value.
+             */
+            constexpr value_type get() const {
+                return value;
+            }
+
+            CCLNODISCARD static constexpr packed_integer make(const value_type high, const value_type low) {
+                CCL_THROW_IF(high > high_part_max, std::out_of_range{"High value too large."});
+                CCL_THROW_IF(low > low_part_max, std::out_of_range{"Low value too large."});
+
+                return (high << high_part_shift_bits) | low;
+            }
+    };
+
+    template<typename ...Args>
+    constexpr bool operator==(const packed_integer<Args...> a, const packed_integer<Args...> b) {
+        return a.get() == b.get();
+    }
+
+    template<typename T, size_t LowPartSize>
+    constexpr bool operator==(const packed_integer<T, LowPartSize> a, const T b) {
+        return a.get() == b;
+    }
+
+    template<typename T, size_t LowPartSize>
+    constexpr bool operator==(const T a, const packed_integer<T, LowPartSize> b) {
+        return a == b.get();
+    }
+}
+
+#endif // CCL_PACKED_INTEGER_HPP
---
+++ test/packed-integer.cpp
@@ -0,0 +1,144 @@
+#include <ccl/test/test.hpp>
+#include <ccl/packed-integer.hpp>
+
+using namespace ccl;
+
+int main(int argc, char **argv) {
+    test_suite suite;
+
+    suite.add_test("ctor (default)", [] () {
+        using my_int = packed_integer<uint64_t>;
+
+        my_int n;
+
+        equals(n.get(), 0ULL);
+        equals(n.high(), 0ULL);
+        equals(n.low(), 0ULL);
+        equals<unsigned, unsigned>(my_int::low_part_size, 32);
+        equals<unsigned, unsigned>(my_int::high_part_shift_bits, 32);
+        equals<size_t, size_t>(my_int::low_part_mask, 0xffffffff);
+    });
+
+    suite.add_test("ctor (default, uneven)", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        equals<unsigned, unsigned>(my_int::low_part_size, 48);
+        equals<unsigned, unsigned>(my_int::high_part_shift_bits, 48);
+        equals<size_t, size_t>(my_int::low_part_mask, 0xffffffffffff);
+        equals<size_t, size_t>(my_int::low_part_max, (1ULL << 48) - 1);
+        equals<size_t, size_t>(my_int::high_part_max, (1ULL << 16) - 1);
+    });
+
+    suite.add_test("ctor (value)", [] () {
+        using my_int = packed_integer<uint64_t>;
+
+        my_int n = 0xaaaaaaaabbbbbbbb;
+
+        equals(n.get(), 0xaaaaaaaabbbbbbbb);
+        equals(n.high(), 0xaaaaaaaa);
+        equals(n.low(), 0xbbbbbbbb);
+    });
+
+    suite.add_test("ctor (copy)", [] () {
+        using my_int = packed_integer<uint64_t>;
+
+        my_int n = 0xaaaaaaaabbbbbbbb;
+        my_int n2 = n;
+
+        equals(n2.get(), 0xaaaaaaaabbbbbbbb);
+        equals(n2.high(), 0xaaaaaaaa);
+        equals(n2.low(), 0xbbbbbbbb);
+    });
+
+    suite.add_test("type cast", [] () {
+        using my_int = packed_integer<uint64_t>;
+
+        my_int n = 0xaaaaaaaabbbbbbbb;
+
+        equals<uint64_t, uint64_t>(n, 0xaaaaaaaabbbbbbbb);
+    });
+
+    suite.add_test("assignment (copy)", [] () {
+        using my_int = packed_integer<uint64_t>;
+
+        my_int n = 0xaaaaaaaabbbbbbbb;
+        my_int n2 = 4;
+
+        n = n2;
+
+        equals<uint64_t, uint64_t>(n, 4);
+    });
+
+    suite.add_test("assignment (value)", [] () {
+        using my_int = packed_integer<uint64_t>;
+
+        my_int n = 0xaaaaaaaabbbbbbbb;
+
+        n = 4;
+
+        equals<uint64_t, uint64_t>(n, 4);
+    });
+
+    suite.add_test("high, low & get", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        my_int n = 0xaaaaccccbbbbbbbb;
+
+        equals<uint64_t, uint64_t>(n.high(), 0xaaaa);
+        equals<uint64_t, uint64_t>(n.low(), 0xccccbbbbbbbb);
+        equals<uint64_t, uint64_t>(n.get(), 0xaaaaccccbbbbbbbb);
+    });
+
+    suite.add_test("comparison (equality, reference)", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        my_int n = 0xaaaaccccbbbbbbbb;
+        my_int n2 = 0xaaaaccccbbbbbbbb;
+        my_int n3 = 0xaaaaccccbbbbbbbc;
+
+        check(n == n2);
+        check(n2 != n3);
+    });
+
+    suite.add_test("comparison (equality/inequality, obj vs value)", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        my_int n = 0xaaaaccccbbbbbbbb;
+
+        check(n == 0xaaaaccccbbbbbbbb);
+        check(n != 0xaaaaccccbbbbbbbd);
+    });
+
+    suite.add_test("comparison (equality/inequality, value vs obj)", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        my_int n = 0xaaaaccccbbbbbbbb;
+
+        check(0xaaaaccccbbbbbbbb == n);
+        check(0xaaaaccccbbbbbbbd != n);
+    });
+
+    suite.add_test("make", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        equals(0xaaaaccccbbbbbbbb, my_int::make(0xaaaa, 0xccccbbbbbbbb));
+    });
+
+    suite.add_test("make (high value too large)", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        throws<std::out_of_range>([] () {
+            const auto x CCLUNUSED = my_int::make(0xccccbbbbbbbb, 0xccccbbbbbbbb);
+        });
+    });
+
+    suite.add_test("make (low value too large)", [] () {
+        using my_int = packed_integer<uint64_t, 48>;
+
+        throws<std::out_of_range>([] () {
+            const auto x CCLUNUSED = my_int::make(0x1, 0xccccbbbbbbbbcc);
+        });
+    });
+
+    return suite.main(argc, argv);
+}
