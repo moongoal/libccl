a6ac4521ddb434861618b68e2c6638ed8ce9e2ff
Alfredo Mungo <mungalfr@amazon.com>
2022-12-20 20:34:25 +0000

    Added ecs registry editor and max entity count check capability

--- include/ccl/ecs/registry.hpp
+++ include/ccl/ecs/registry.hpp
@@ -8,6 +8,7 @@
 
 #include <ccl/api.hpp>
 #include <ccl/debug.hpp>
+#include <ccl/exceptions.hpp>
 #include <ccl/ecs/archetype.hpp>
 #include <ccl/ecs/entity.hpp>
 #include <ccl/ecs/view.hpp>
@@ -15,11 +16,20 @@
 #include <ccl/dense-map.hpp>
 
 namespace ccl::ecs {
+    namespace internal {
+        template<basic_allocator Allocator>
+        class registry_editor;
+    }
+
     template<basic_allocator Allocator>
     class registry {
+        friend class internal::registry_editor<Allocator>;
+
         public:
             using allocator_type = Allocator;
             using archetype = ccl::ecs::archetype<Allocator>;
+
+            static constexpr entity_id_t max_entity_id = entity_t::underlying_type::low_part_max;
 
         private:
             entity_id_t next_entity_id = 0;
@@ -28,11 +38,17 @@ namespace ccl::ecs {
         public:
             /**
              * Add a new entity to this registry. The returned entity is guaranteed
-             * To be new, unique and of the 0th generation.
+             * To be new, unique and of the 0th generation. Will throw an `std::out_of_range`
+             * exception if the maximum allowed number of entities is reached.
              *
              * @return The newly created entity.
              */
-            entity_t add_entity() {
+            CCLNODISCARD entity_t add_entity() {
+                CCL_THROW_IF(
+                    next_entity_id >= max_entity_id,
+                    std::out_of_range{"Maximum number of entities reached."}
+                );
+
                 return entity_t::make(0, next_entity_id++);
             }
 
--- test/ecs/registry.cpp
+++ test/ecs/registry.cpp
@@ -1,11 +1,13 @@
 #include <ccl/test/test.hpp>
 #include <ccl/test/counting-test-allocator.hpp>
 #include <ccl/ecs/registry.hpp>
+#include <ccl/ecs/registry-editor.hpp>
 
 using namespace ccl;
 using namespace ccl::ecs;
 
 using test_registry = registry<counting_test_allocator>;
+using test_registry_editor = ccl::ecs::internal::registry_editor<test_registry::allocator_type>;
 
 int main(int argc, char **argv) {
     test_suite suite;
@@ -19,6 +21,17 @@ int main(int argc, char **argv) {
         differs(e1, e2);
         equals(e1.generation(), 0U);
         check(e1.value() < e2.value());
+    });
+
+    suite.add_test("add_entity (max entities)", [] () {
+        test_registry registry;
+        test_registry_editor editor{registry};
+
+        editor.set_next_entity_id(test_registry::max_entity_id);
+
+        throws<std::out_of_range>([&registry] () {
+            const auto x CCLUNUSED = registry.add_entity();
+        });
     });
 
     return suite.main(argc, argv);
