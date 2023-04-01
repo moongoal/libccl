6e9ac7fe1e6992b14bc4446a304413390ed02908
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2023-04-01 21:13:25 +0100

    Added composite allocator tests

--- test/memory/composite-allocator.cpp
+++ test/memory/composite-allocator.cpp
@@ -44,6 +44,17 @@ int main(int argc, char **argv) {
         check(allocator2.owns(ptr2));
     });
 
+    suite.add_test("allocate third", [] () {
+        test_local_allocator allocator1, allocator2;
+        test_composite_allocator composite{allocator1, allocator2};
+
+        const void * const ptr1 = composite.allocate(1);
+        const void * const ptr2 = composite.allocate(16);
+
+        check(allocator1.owns(ptr1));
+        check(allocator2.owns(ptr2));
+    });
+
     suite.add_test("deallocate first", [] () {
         spy_allocator allocator1, allocator2;
         spied_composite_allocator composite{allocator1, allocator2};
@@ -60,6 +71,35 @@ int main(int argc, char **argv) {
 
         composite.deallocate(nullptr);
         check(allocator2.deallocated);
+    });
+
+    suite.add_test("owns", [] () {
+        test_local_allocator allocator1, allocator2;
+        test_composite_allocator composite{allocator1, allocator2};
+
+        const void * const ptr1 = composite.allocate<uint8_t>(15);
+        const void * const ptr2 = composite.allocate<uint8_t>(2);
+
+        equals(composite.owns(ptr1), false);
+        equals(composite.owns(ptr2), false);
+    });
+
+    suite.add_test("get_features", [] () {
+        test_local_allocator allocator1, allocator2;
+        test_composite_allocator composite{allocator1, allocator2};
+
+        equals(composite.get_features() & CCL_ALLOCATOR_FEATURE_OWNERSHIP_QUERY_BIT, false);
+    });
+
+    suite.add_test("get_allocation_info", [] () {
+        test_local_allocator allocator1, allocator2;
+        test_composite_allocator composite{allocator1, allocator2};
+
+        const void * const ptr1 = composite.allocate<uint8_t>(15);
+        const auto info = composite.get_allocation_info(ptr1);
+
+        equals(info.alignment, 0);
+        equals(info.size, 0);
     });
 
     return suite.main(argc, argv);
