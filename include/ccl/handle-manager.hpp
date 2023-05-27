813cf65b9c68292b2d6a1cbad7ede7e5df62d793
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2023-05-28 00:48:59 +0100

    Fixed bugs and added tests.

--- include/ccl/handle-manager.hpp
+++ include/ccl/handle-manager.hpp
@@ -58,7 +58,7 @@ namespace ccl {
             using value_type = typename handle_type::value_type;
             using vector_type = paged_vector<value_type, value_type*, allocator_type>;
 
-            static constexpr value_type value_unused_mask = bitcount(static_cast<value_type>(0) - 1) - 1;
+            static constexpr value_type value_unused_mask = handle_type::max_generation + 1;
             static constexpr handle_manager_expiry_policy expiry_policy = ExpiryPolicy;
 
         private:
@@ -121,7 +121,7 @@ namespace ccl {
                     }
                 };
 
-                const auto result = std::find_if(
+                auto result = std::find_if(
                     handles.begin() + last_slot_index,
                     handles.end(),
                     is_available_slot
@@ -131,11 +131,15 @@ namespace ccl {
                     return result;
                 }
 
-                return std::find_if(
+                const auto new_end = handles.begin() + last_slot_index;
+
+                result = std::find_if(
                     handles.begin(),
-                    handles.begin() + last_slot_index,
+                    new_end,
                     is_available_slot
                 );
+
+                return result != new_end ? result : handles.end();
             }
 
         public:
@@ -148,16 +152,16 @@ namespace ccl {
                 typename vector_type::iterator result = find_next_slot();
 
                 if(result == handles.end()) {
-                    const size_t size = handles.size();
+                    last_slot_index = handles.size();
 
                     add_page();
-                    result = handles.begin() + size;
+                    result = handles.begin() + last_slot_index;
                 }
 
                 const value_type generation = get_slot_generation(*result);
                 const value_type index = last_slot_index;
 
-                last_slot_index = (result - handles.begin() + 1) & (vector_type::page_size - 1);
+                last_slot_index = (result - handles.begin() + 1) & (handles.size() - 1);
                 *result = generation;
 
                 return handle_type::make(generation, index);
@@ -175,7 +179,7 @@ namespace ccl {
              *
              * @return True if the handle is valid, false if it's invalid.
              */
-            bool validate_handle(const handle_type handle) const {
+            bool is_valid_handle(const handle_type handle) const {
                 const auto index = handle.value();
                 const auto generation = handle.generation();
 
@@ -198,28 +202,38 @@ namespace ccl {
              * @param handle The handle to release.
              */
             void release(const handle_type handle) {
-                CCL_THROW_IF(!validate_handle(handle), std::invalid_argument{"Invalid handle."});
+                CCL_THROW_IF(!is_valid_handle(handle), std::invalid_argument{"Invalid handle."});
 
                 const auto index = handle.value();
                 const auto generation = handle.generation();
 
                 if constexpr(expiry_policy == handle_manager_expiry_policy::recycle) {
-                    handles[index] = (generation + 1) & handle_type::max_generation;
+                    handles[index] = ((generation + 1) & handle_type::max_generation) | value_unused_mask;
                 } else {
-                    handles[index] = generation + 1;
+                    handles[index] = (generation + 1) | value_unused_mask;
                 }
             }
 
             /**
              * Resets all expired handles to the 0th generation so they can be used again.
              */
-            std::enable_if<expiry_policy == handle_manager_expiry_policy::discard>
+            template<bool Enable = expiry_policy == handle_manager_expiry_policy::discard>
+            std::enable_if_t<Enable>
             reset_expired() {
                 std::for_each(handles.begin(), handles.end(), [] (value_type& slot) {
                     if(!is_slot_used(slot) && get_slot_generation(slot) == handle_type::max_generation) {
                         slot = value_unused_mask;
                     }
                 });
+            }
+
+            /**
+             * Reset all handles to the 0th generation. This is equivalent to
+             * creating a new handle manager.
+             */
+            void reset() {
+                std::fill(handles.begin(), handles.end(), value_unused_mask);
+                last_slot_index = 0;
             }
     };
 }
--- test/handle-manager.cpp
+++ test/handle-manager.cpp
@@ -22,6 +22,19 @@ int main(int argc, char **argv) {
         differs(handle1, handle2);
     });
 
+    suite.add_test("release", [] () {
+        test_recycle_handle_manager manager;
+
+        for(size_t i = 0; i < test_recycle_handle_manager::vector_type::page_size; ++i) {
+            manager.release(manager.acquire());
+        }
+
+        const auto handle = manager.acquire();
+
+        equals(handle.generation(), 1);
+        equals(handle.value(), 0);
+    });
+
     suite.add_test("acquire/release cycle", [] () {
         test_recycle_handle_manager manager;
 
@@ -34,17 +47,81 @@ int main(int argc, char **argv) {
         differs(handle2, handle3);
     });
 
-    suite.add_test("release", [] () {
+    suite.add_test("reset", []() {
         test_recycle_handle_manager manager;
 
-        for(size_t i = 0; i < test_recycle_handle_manager::vector_type::page_size; ++i) {
+        const auto handle1 = manager.acquire();
+        manager.reset();
+        const auto handle2 = manager.acquire();
+
+        equals(handle1, handle2);
+    });
+
+    suite.add_test("is_valid_handle", []() {
+        test_recycle_handle_manager manager;
+
+        const auto handle1 = manager.acquire();
+        const auto handle2 = manager.acquire();
+
+        manager.release(handle1);
+
+        check(!manager.is_valid_handle(handle1));
+        check(manager.is_valid_handle(handle2));
+    });
+
+    suite.add_test("handle expiry (recycle)", []() {
+        test_recycle_handle_manager manager;
+
+        /*
+         * +1 because the highest generation value is
+         * a valid generation in recycle mode.
+         */
+        const size_t total_iterations =
+            test_recycle_handle_manager::vector_type::page_size
+            * (test_recycle_handle_manager::handle_type::max_generation + 1);
+
+        for(size_t i = 0; i < total_iterations; ++i) {
             manager.release(manager.acquire());
         }
 
         const auto handle = manager.acquire();
 
-        equals(handle.generation(), 1);
-        equals(handle.value(), 0);
+        equals(handle.raw(), 0);
+    });
+
+    suite.add_test("handle expiry (discard)", []() {
+        test_discard_handle_manager manager;
+
+        const size_t total_iterations =
+            test_recycle_handle_manager::vector_type::page_size
+            * test_recycle_handle_manager::handle_type::max_generation;
+
+        for(size_t i = 0; i < total_iterations; ++i) {
+            manager.release(manager.acquire());
+        }
+
+        const auto handle = manager.acquire();
+
+        equals(handle.generation(), 0);
+        equals(handle.value(), test_recycle_handle_manager::vector_type::page_size);
+    });
+
+    suite.add_test("reset_expired", [] () {
+        test_discard_handle_manager manager;
+
+        const size_t total_iterations =
+            test_recycle_handle_manager::vector_type::page_size
+            * test_recycle_handle_manager::handle_type::max_generation;
+
+        for(size_t i = 0; i < total_iterations; ++i) {
+            manager.release(manager.acquire());
+        }
+
+        manager.reset_expired();
+
+        const auto handle = manager.acquire();
+
+        equals(handle.raw(), 0);
     });
 
     return suite.main(argc, argv);
