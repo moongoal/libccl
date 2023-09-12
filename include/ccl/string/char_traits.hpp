cc7585dddda14c5650a6893191ce4b4a920d1ced
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2023-09-12 23:13:47 +0100

    Added missing standard traits

--- include/ccl/string/char_traits.hpp
+++ include/ccl/string/char_traits.hpp
@@ -6,14 +6,21 @@
 #ifndef CCL_STRING_CHAR_TRAITS_HPP
 #define CCL_STRING_CHAR_TRAITS_HPP
 
+#include <cstdio>
 #include <cstring>
 #include <ccl/api.hpp>
 #include <ccl/type-traits.hpp>
+#include <ccl/util.hpp>
 
 namespace ccl {
     template<typename CharType>
     struct char_traits;
 
+    /**
+     * Basic char traits.
+     *
+     * @see https://en.cppreference.com/w/cpp/named_req/CharTraits
+     */
     template<typename CharType, typename IntType, typename PosType>
     struct base_char_traits {
         using char_type = CharType;
@@ -43,10 +50,10 @@ namespace ccl {
                 ::memmove(dest, src, count);
             } else {
                 for(pos_type i = count; i > 0; --i) {
-                    dest[i] = src[i];
+                    assign(dest[i], src[i]);
                 }
 
-                dest[0] = src[0];
+                assign(dest[0], src[0]);
             }
 
             return dest;
@@ -57,7 +64,7 @@ namespace ccl {
                 ::memcpy(dest, src, count);
             } else {
                 for(pos_type i = 0; i < count; ++i) {
-                    dest[i] = src[i];
+                    assign(dest[i], src[i]);
                 }
             }
 
@@ -65,17 +72,64 @@ namespace ccl {
         }
 
         static constexpr int compare(const char_type* const s1, const char_type* const s2, const pos_type count) {
-            if constexpr(std::is_trivial_v<char_type>) {
-                return ::strncmp(s1, s2, count);
-            } else {
-                for(pos_type i = 0; i < count; ++i) {
-                    if(s1[i] != s2[i]) {
-                        return false;
-                    }
+            bool full_equality = false;
+
+            for(pos_type i = 0; i < count; ++i) {
+                const char_type &c1 = s1[i];
+                const char_type &c2 = s2[i];
+
+                if(and_(full_equality, lt(c1, c2))) {
+                    return -1;
                 }
 
-                return true;
+                full_equality &= eq(c1, c2);
             }
+
+            return 1 - static_cast<int>(full_equality);
+        }
+
+        static constexpr std::size_t length(const char_type* s) {
+            pos_type i = 0;
+
+            for(; compare(s[i], char_type{}); ++i);
+
+            return i;
+        }
+
+        static constexpr const char_type* find(const char_type* const ptr, const pos_type count, const char_type& ch) {
+            for(pos_type i = 0; i < count; ++i) {
+                const char_type &cur = &ptr[count];
+
+                if(compare(cur, ch) == 0) {
+                    return &cur;
+                }
+            }
+
+            return nullptr;
+        }
+
+        static constexpr char_type to_char_type(const int_type c) noexcept {
+            return static_cast<char_type>(c);
+        }
+
+        static constexpr int_type to_int_type(const char_type c) noexcept {
+            return static_cast<int_type>(c);
+        }
+
+        static constexpr bool eq_int_type(const int_type c1, const int_type c2) noexcept {
+            return c1 == c2;
+        }
+
+        static constexpr int_type eof() noexcept {
+            return EOF;
+        }
+
+        static constexpr int_type not_eof(const int_type e) noexcept {
+            return choose(
+                e,
+                EOF - 1,
+                eq_int_type(e, eof())
+            );
         }
     };
 
